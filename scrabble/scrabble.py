ecd81b83133dee304ef0c017001dde736ba87776
MarekWB <44985945+MarekWB@users.noreply.github.com>
2018-11-14 12:11:04 +0100

    Update scrabble.py

--- scrabble/scrabble.py
+++ scrabble/scrabble.py
@@ -1,3 +1,5 @@
+#projekt dot. zadań z gry Scrablle w oparciu o polecenia z linii komend.
+
 scrabble_scores = [(1, "E A O I N R T L S U"), (2, "D G"), (3, "B C M P"),(4, "F H V W Y"), (5, "K"), (8, "J X"), (10, "Q Z")]
 
 letter_score = {letter: score for score, letters in scrabble_scores for letter in letters.split()}
